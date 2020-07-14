e0ff8a51fb518ac1163257a431e43cc97b77730b
Denis <denys.hrulov@teamdev.com>
2020-07-14 18:43:32 +0300

    add uint-tests for basic classes

--- tests/TestTaskEntity.cpp
+++ tests/TestTaskEntity.cpp
@@ -2,3 +2,31 @@
 // Created by denis on 14.07.20.
 //
 
+#include <gtest/gtest.h>
+#include "../src/memory_model/TaskEntity.h"
+#include "../src/memory_model/TaskEntity.cpp"
+
+class TaskEntityTest : public ::testing::Test {
+
+};
+
+TEST_F(TaskEntityTest, noRefrenceCorruption) {
+    Task t {"name", Task::Priority::NONE, "lbl", 2020};
+    TaskEntity x(1, t);
+    auto y = x;
+    EXPECT_EQ(x.getName(), "name");
+    EXPECT_EQ(x.getPriority(), Task::Priority::NONE);
+    EXPECT_EQ(x.getLabel(), "lbl");
+    EXPECT_EQ(x.getDate(), 2020);
+}
+
+TEST_F(TaskEntityTest, correctCopy) {
+    Task t {"name", Task::Priority::NONE, "lbl", 2020};
+    TaskEntity x(1, t);
+    auto y = x;
+    EXPECT_EQ(y.getId(), 1);
+    EXPECT_EQ(y.getName(), "name");
+    EXPECT_EQ(y.getPriority(), Task::Priority::NONE);
+    EXPECT_EQ(y.getLabel(), "lbl");
+    EXPECT_EQ(y.getDate(), 2020);
+}
--- tests/TestTaskStorage.cpp
+++ tests/TestTaskStorage.cpp
@@ -2,3 +2,20 @@
 // Created by denis on 14.07.20.
 //
 
+#include <gtest/gtest.h>
+#include "../src/memory_model/TaskStorage.h"
+#include "../src/memory_model/TaskStorage.cpp"
+
+class TaskStorageTest : public ::testing::Test {
+
+};
+
+TEST_F(TaskStorageTest, ptrIsValid) {
+    TaskStorage ts;
+    auto ptask = ts.createTask("name", Task::Priority::NONE, "lbl", 2020);
+    ASSERT_TRUE(ptask);
+    EXPECT_EQ(ptask->name, "name");
+    EXPECT_EQ(ptask->priority, Task::Priority::NONE);
+    EXPECT_EQ(ptask->label, "lbl");
+    EXPECT_EQ(ptask->date, 2020);
+}
