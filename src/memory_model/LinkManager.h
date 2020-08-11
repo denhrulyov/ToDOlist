eea2c0729baf1a0914ee9804a06f33107971e95b
denis <denys.hrulov@teamdev.com>
2020-08-11 12:32:24 +0300

    Create interface for LinkManager

--- src/memory_model/LinkManager.h
+++ src/memory_model/LinkManager.h
@@ -5,20 +5,20 @@
 #ifndef TODOLIST_LINKMANAGER_H
 #define TODOLIST_LINKMANAGER_H
 
-#include "TaskNode.h"
+#include "LinkManagerInterface.h"
 #include "PriorityViewInterface.h"
 
-class LinkManager {
+class LinkManager : public LinkManagerInterface {
 
 public:
     LinkManager(PriorityViewInterface<BoostDate>& by_time,
                 PriorityViewInterface<std::string>& by_label);
 
 public:
-    void linkSubTask(const std::weak_ptr<TaskNode>& main_task, const std::weak_ptr<TaskNode>& sub_task);
-    void setLinks(const std::weak_ptr<TaskNode>& node);
-    void removeLinks(const std::weak_ptr<TaskNode>& node);
-    void moveInboundLinks(const std::weak_ptr<TaskNode>& from, const std::weak_ptr<TaskNode>& to);
+    void linkSubTask(const std::weak_ptr<TaskNode>& main_task, const std::weak_ptr<TaskNode>& sub_task) override;
+    void setLinks(const std::weak_ptr<TaskNode>& node) override;
+    void removeLinks(const std::weak_ptr<TaskNode>& node) override;
+    void moveInboundLinks(const std::weak_ptr<TaskNode>& from, const std::weak_ptr<TaskNode>& to) override;
 
 private:
     PriorityViewInterface<BoostDate> &by_time_;
