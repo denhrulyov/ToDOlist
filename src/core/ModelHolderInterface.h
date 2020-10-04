f9e8d817e8bf70c380243a5fd7f640299657baf5
Denis <denys.hrulov@teamdev.com>
2020-10-04 21:30:34 +0300

    Create model holder

---
+++ src/core/ModelHolderInterface.h
@@ -0,0 +1,19 @@
+//
+// Created by denis on 04.10.20.
+//
+
+#ifndef TODOLIST_MODELHOLDERINTERFACE_H
+#define TODOLIST_MODELHOLDERINTERFACE_H
+
+#include "memory_model/api/TaskModelInterface.h"
+
+class ModelHolderInterface {
+
+public:
+    virtual TaskModelInterface&     GetModel() = 0;
+    virtual bool                    LoadModelFromFile(const std::string& filepath) = 0;
+    virtual bool                    SaveModelToFile(const std::string& filepath) = 0;
+};
+
+
+#endif //TODOLIST_MODELHOLDERINTERFACE_H
