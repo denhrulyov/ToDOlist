361bf2c7046c45815e8fec45ad746c252478bea2
Denis <denys.hrulov@teamdev.com>
2020-10-28 18:03:35 +0200

    Implement GrpcTaskServiceAdaptor

---
+++ src/core/api/GrpcTaskServiceAdaptor.h
@@ -0,0 +1,69 @@
+//
+// Created by denis on 27.10.20.
+//
+
+#ifndef TODOLIST_GRPCTASKSERVICEADAPTOR_H
+#define TODOLIST_GRPCTASKSERVICEADAPTOR_H
+
+#include "core/persistence/ProtoConvert.h"
+#include "TaskServiceInterface.h"
+#include "TaskService.grpc.pb.h"
+#include "TaskService.pb.h"
+#include <grpcpp/grpcpp.h>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+class GrpcTaskServiceAdaptor : public TaskServiceInterface {
+public:
+    explicit GrpcTaskServiceAdaptor(std::unique_ptr<GrpcTaskService::Stub> stub)
+    :
+    stub_(std::move(stub))
+    {}
+
+public:
+    std::vector<TaskDTO>                                    getToday() override;
+    std::vector<TaskDTO>                                    getThisWeek() override;
+    std::vector<TaskDTO>                                    getAllTasks() override;
+    std::vector<TaskDTO>                                    getAllWithLabel(const std::string& label) override;
+    std::optional<TaskDTO>                                  getTaskByID(TaskID id) override;
+    std::vector<TaskDTO>                                    getSubTasks(TaskID id) override;
+    std::vector<TaskDTO>                                    getSubTasksRecursive(TaskID id) override;
+
+public:
+    TaskCreationResult                                      addTask(const TaskDTO &task_data) override;
+    TaskCreationResult                                      addSubTask(TaskID parent, const TaskDTO &task_data) override;
+    TaskModificationResult                                  deleteTask(TaskID id) override;
+    TaskModificationResult                                  postponeTask(TaskID id, BoostDate) override;
+    RequestResult                                           complete(TaskID id) override;
+
+public:
+    RequestResult                                           saveToFile(const std::string& filepath) override;
+    RequestResult                                           loadFromFile(const std::string& filepath) override;
+
+public:
+    ~GrpcTaskServiceAdaptor() override =                    default;
+
+private:
+    std::unique_ptr<GrpcTaskService::Stub>                  stub_;
+};
+
+std::vector<TaskDTO>    RestoreFromGrpcDTOList(const TaskDTOList& all);
+TaskDTO                 RestoreFromGrpcDTO(const GrpcTaskDTO& all);
+template<class Result>
+Result                  RestoreResult(const DefaultResponse& response);
+TaskCreationResult      RestoreResult(const AddTaskResponse& response);
+
+
+/**************************************************************************************************************/
+
+template<class Result>
+Result RestoreResult(const DefaultResponse& response) {
+    bool status =         response.success();
+    auto error_msg =      status ? std::make_optional(response.error_msg()) : std::nullopt;
+    return Result(status, error_msg);
+}
+
+
+#endif //TODOLIST_GRPCTASKSERVICEADAPTOR_H
--- src/core/persistence/ProtoConvert.h
+++ src/core/persistence/ProtoConvert.h
@@ -6,13 +6,15 @@
 #define TODOLIST_PROTOCONVERT_H
 
 #include "core/memory_model/api/RepositoryTaskDTO.h"
+#include "core/api/TaskDTO.h"
 #include "task.pb.h"
 
 namespace proto_convert {
 
     RepositoryTaskDTO RestoreFromMessage(const TaskData &message);
 
-    bool WriteToMessage(const RepositoryTaskDTO &data, TaskData *message);
+    template<class TaskDTO>
+    bool WriteToMessage(const TaskDTO &task, TaskData *message);
 
     std::unique_ptr<google::protobuf::Timestamp> GetProtobufDate(const BoostDate &date);
 
@@ -21,6 +23,16 @@ namespace proto_convert {
     TaskData::Priority GetProtobufPriority(TaskPriority prior);
 
     TaskPriority RestorePriority(TaskData::Priority prior);
+}
+
+template<class TaskDTO>
+bool proto_convert::WriteToMessage(const TaskDTO &task, TaskData *message) {
+    message->set_name(task.getName());
+    message->set_prior(GetProtobufPriority(task.getPriority()));
+    message->set_label(task.getLabel());
+    message->set_allocated_date(GetProtobufDate(task.getDate()).release());
+    message->set_completed(task.isCompleted());
+    return true;
 }
 
 #endif //TODOLIST_PROTOCONVERT_H
